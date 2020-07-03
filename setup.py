5f3f3d9c8c2e1ebcf0d5b628dfb995df03fee7a0
aleX <xintong.wang@riotinto.com>
2020-07-03 11:39:03 -0400

    changed package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="logutils", # Replace with your own username
+    name="logutils-minimal", # Replace with your own username
     version="0.0.1",
     author="Alex X.T. Wang",
     author_email="xintong.wang@riotinto.com",
