3b01e8f9151a3093accf3b590780d12bc6375ff3
AlexXTW <66092535+AlexXTW@users.noreply.github.com>
2020-07-03 14:18:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="logging_utilities", # Replace with your own username
+    name="logging_utils", # Replace with your own username
     version="0.0.1",
     author="Alex X.T. Wang",
     author_email="xintong.wang@riotinto.com",
     description="Utilities package for function logging",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AlexXTW/logutils",
+    url="https://github.com/AlexXTW/logging_utils",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
